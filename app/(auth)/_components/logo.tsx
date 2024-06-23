d356625c8a5f5470010bc692e2ee60e7abbb1b74
smayank1214 <rawatmayank716@gmail.com>
2024-06-23 11:35:06 +0530

    Add dark mode

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import logo from '@/public/streamx_logo.png'
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-4">
+        <Image
+          src={logo}
+          alt="StreamX"
+          height="80"
+          width="80"
+        />
+      </div>
+      <div className={cn(
+        "flex flex-col items-center",
+        font.className,
+      )}>
+        <p className="text-3xl text-white font-semibold">
+          StreamX
+        </p>
+        <p className="text-sm text-white text-muted-foreground">
+          Let's Stream
+        </p>
+      </div>
+    </div>
+  );
+};
