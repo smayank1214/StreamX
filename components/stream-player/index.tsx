6ec8a1008eb5c5636bf8fa934162fc8189ce2862
smayank1214 <rawatmayank716@gmail.com>
2024-06-26 10:09:29 +0530

    Header and Info card setup

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,12 +7,12 @@ import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 
-// import { InfoCard } from "./info-card";
+import { InfoCard } from "./info-card";
 // import { AboutCard } from "./about-card";
 import { ChatToggle } from "./chat-toggle";
 import { Chat, ChatSkeleton } from "./chat";
 import { Video, VideoSkeleton } from "./video";
-// import { Header, HeaderSkeleton } from "./header";
+import { Header, HeaderSkeleton } from "./header";
 
 type CustomStream = {
   id: string;
@@ -75,20 +75,20 @@ export const StreamPlayer = ({
             hostName={user.username}
             hostIdentity={user.id}
           />
-          {/* <Header
+          <Header
             hostName={user.username}
             hostIdentity={user.id}
             viewerIdentity={identity}
             imageUrl={user.imageUrl}
             isFollowing={isFollowing}
             name={stream.name}
-          /> */}
-          {/* <InfoCard
+          />
+          <InfoCard
             hostIdentity={user.id}
             viewerIdentity={identity}
             name={stream.name}
             thumbnailUrl={stream.thumbnailUrl}
-          /> */}
+          />
           {/* <AboutCard
             hostName={user.username}
             hostIdentity={user.id}
@@ -123,7 +123,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/* <HeaderSkeleton /> */}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
